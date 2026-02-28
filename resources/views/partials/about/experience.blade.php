49e4a888acdc7cd0dcdb169f8440dce4273a0aba
a-mamal <mamalikidoua@gmail.com>
2026-02-28 09:56:11 +0200

    refactor(about): update experience section to use card classes

--- resources/views/partials/about/experience.blade.php
+++ resources/views/partials/about/experience.blade.php
@@ -1,11 +1,10 @@
 <section id="experience" class="about-section">
     <h2>Experience</h2>
 
-    <div class="cards-grid">
-        <article class="card">
-            <h3>Job Title</h3>
-            <p>Company Name </p>
-            <p>Brief description of responsibilities...</p>
+        <article class="card experience-card">
+            <h3 class="card-title">Job Title</h3>
+            <p class="card-meta">Company Name </p>
+            <p class="card-text">Brief description of responsibilities...</p>
         </article>
-    </div>
+        
 </section>
