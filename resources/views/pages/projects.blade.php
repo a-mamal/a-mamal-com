d405356d287809f5244eb4b2fd524eee6efaf637
a-mamal <mamalikidoua@gmail.com>
2026-02-28 16:05:31 +0200

    refactor(css/html): wrap projects in card-grid container for layout reuse

--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -9,57 +9,59 @@
 
     <section class="projects-section">
         <h2>All projects</h2>
+        <div class="card-grid">
+            @forelse($projects as $project)
+            
+                <article class=" card project-card">
+                    {{-- Title  --}}
+                    <h3 class="card-title">{{ $project->title }}</h3>
 
-        @forelse($projects as $project)
-
-            <article class=" card project-card">
-                {{-- Title  --}}
-                <h3 class="card-title">{{ $project->title }}</h3>
-
-                {{-- Type --}}
-                @if($project->type)
-                    <p class="card-meta">{{ $project->type }}</p>
-                @endif
-
-                {{-- Description --}}
-                @if($project->description)
-                    <p class="card-text">{{ $project->description }}</p>
-                @endif
-
-                {{-- Highlights --}}
-                @if($project->highlights)
-                    <ul class="card-list">
-                        @foreach($project->highlights as $highlight)
-                            <li class="card-list-item">{!! $highlight !!}</li>
-                        @endforeach
-                    </ul>
-                @endif
-
-                {{-- Links --}}
-                <div class="card-actions">
-                    @if($project->github_url)
-                        <a  href="{{ $project->github_url }}"
-                            class="button" 
-                            target="_blank">
-                            GitHub
-                        </a>
+                    {{-- Type --}}
+                    @if($project->type)
+                        <p class="card-meta">{{ $project->type }}</p>
                     @endif
 
-                    @if($project->project_url)
-                        <a  href="{{ $project->project_url }}" 
-                            class="button"
-                            target="_blank">
-                            Live Demo
-                        </a>
+                    {{-- Description --}}
+                    @if($project->description)
+                        <p class="card-text">{{ $project->description }}</p>
                     @endif
 
-                </div>
-            </article>
+                    {{-- Highlights --}}
+                    @if($project->highlights)
+                        <ul class="card-list">
+                            @foreach($project->highlights as $highlight)
+                                <li class="card-list-item">{!! $highlight !!}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+
+                    {{-- Links --}}
+                    <div class="card-actions">
+                        @if($project->github_url)
+                            <a  href="{{ $project->github_url }}"
+                                class="button" 
+                                target="_blank">
+                                GitHub
+                            </a>
+                        @endif
+
+                        @if($project->project_url)
+                            <a  href="{{ $project->project_url }}" 
+                                class="button"
+                                target="_blank">
+                                Live Demo
+                            </a>
+                        @endif
+
+                    </div>
+                </article>
+            
 
         @empty
                 <p>No projects available at the moment. Check back soon!</p>
         @endforelse
-
+        
+        </div>
     </section>
 
 </x-site-layout>
