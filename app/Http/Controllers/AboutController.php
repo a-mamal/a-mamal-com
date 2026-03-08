e9867a84f646421f8440dcdd64a82ba669299797
Anastasia Mamalikidou <contact@a-mamal.com>
2026-03-08 12:05:47 +0200

    feat(about): render spoken languages dynamically (#112)
    * feat(about): add spoken languages to AboutController
    
    * feat(about): render spoken languages dynamically
    
    * style(about): add basic styling for spoken languages list

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -9,11 +9,18 @@ use Carbon\Carbon;
 class AboutController extends Controller
 {
     public function index()
-    {
-        $user = User::with('profiles.links' , 'profiles.degrees.organization')->first();
+    {   
+        // Retrieve the first user and eagerly load related data needed for the About page
+        $user = User::with(
+            'profiles.links' , 
+            'profiles.degrees.organization',
+            'profiles.spokenLanguages'
+            )->first();
+
+        // Select the first profile associated with the user
         $profile = $user?->profiles->first();
 
-        // Format degrees
+        // Format degree dates for display on the About page
         $degrees = ($profile?->degrees ?? collect())->map(function($degree) {
             $degree->formatted_start = $degree->start_date 
                 ? Carbon::parse($degree->start_date)->format('M Y') 
@@ -24,7 +31,7 @@ class AboutController extends Controller
             return $degree;
         });
 
-        // Format certificates
+        // Format certificate award dates for display
         $certificates = ($profile?->certificates ?? collect())->map(function($cert) {
             $cert->formatted_date = $cert->date_awarded 
                 ? Carbon::parse($cert->date_awarded)->format('M Y') 
@@ -32,11 +39,16 @@ class AboutController extends Controller
             return $cert;
         });
 
+        // Retrieve spoken languages for the profile
+        $languages = $profile?->spokenLanguages ?? collect();
+
+        // Pass all prepared data to the About page view
         return view('pages.about', [
             'user' => $user,
             'profile' => $profile,
             'degrees' => $degrees,
-            'certificates' => $certificates
+            'certificates' => $certificates,
+            'languages' => $languages
         ]);
     }
 }
