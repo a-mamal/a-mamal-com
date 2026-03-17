9835d30029dbb0701e22df133a86334e13750100
Aditya Ashok Jadhav <adityajadhav11193@gmail.com>
2026-03-17 18:42:50 +0530

    fix(contact): use profile->links and reusable icons from public/images

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -91,15 +91,14 @@
     </form>
 
 </section>
-
 {{-- Profile Links Section --}}
-@if(isset($profileLinks) && $profileLinks->count())
+@if(isset($profile->links) && $profile->links->count())
     <div class="profile-links-section">
 
         <h3>Find me online</h3>
 
         <div class="profile-links">
-            @foreach($profileLinks as $link)
+            @foreach($profile->links as $link)
                 <a
                     href="{{ $link->url }}"
                     class="profile-link"
@@ -109,12 +108,15 @@
                 >
                     <span class="profile-link-icon">
                         @if($link->platform === 'github')
-                            <svg viewBox="0 0 24 24" width="18" height="18">
-                                <path fill="currentColor" d="M12 .5C5.65.5.5 5.65.5 12c0 5.08 3.29 9.38 7.84 10.9.57.1.78-.25.78-.55v-2.1c-3.19.69-3.87-1.37-3.87-1.37-.52-1.34-1.27-1.69-1.27-1.69-1.04-.72.08-.71.08-.71 1.15.08 1.76 1.18 1.76 1.18 1.02 1.75 2.67 1.25 3.32.96.1-.74.4-1.25.73-1.54-2.55-.29-5.23-1.28-5.23-5.69 0-1.26.45-2.3 1.18-3.11-.12-.29-.51-1.46.11-3.04 0 0 .97-.31 3.18 1.19a11.1 11.1 0 0 1 5.79 0c2.21-1.5 3.18-1.19 3.18-1.19.62 1.58.23 2.75.11 3.04.73.81 1.18 1.85 1.18 3.11 0 4.42-2.69 5.39-5.25 5.67.41.35.77 1.03.77 2.08v3.08c0 .3.21.65.79.54A11.5 11.5 0 0 0 23.5 12C23.5 5.65 18.35.5 12 .5z"/>
-                            </svg>
+                            <img src="{{ asset('images/GitHub_Invertocat_Black.svg') }}" alt="GitHub" width="18" height="18">
+                        @elseif($link->platform === 'linkedin')
+                            <img src="{{ asset('images/InBug-Black.png') }}" alt="LinkedIn" width="18" height="18">
+                        @elseif($link->platform === 'fcc')
+                            <img src="{{ asset('images/fcc_primary_small.svg') }}" alt="FreeCodeCamp" width="18" height="18">
+                        @elseif($link->platform === 'links')
+                            <img src="{{ asset('images/link.svg') }}" alt="Link" width="18" height="18">
                         @endif
                     </span>
-
                     <span class="profile-link-text">
                         {{ ucfirst($link->platform) }}
                     </span>
