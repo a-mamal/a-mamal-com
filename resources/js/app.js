4aa83d89215ea159043368228b57d7bdd3e0d66d
a-mamal <mamalikidoua@gmail.com>
2026-04-26 11:20:24 +0300

    chore(vite): migrate theme switcher to Vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,16 @@
+// Laravel/Vite base setup
 import './bootstrap';
 
+// Import Alpine.js
 import Alpine from 'alpinejs';
 
+// Expose Alpine globally
 window.Alpine = Alpine;
 
+// Activate Alpine so x-data, x-show, x-on work in HTML
 Alpine.start();
+
+// ---- Import custom UI modules ----
+
+// Theme switching (day/night mode + persistence)
+import './theme-switcher';
